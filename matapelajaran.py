4ecb3c94fadac0fb1b80ae7807c3e26ce1bcdf0f
Anggamaul <44119803+Anggamaul@users.noreply.github.com>
2018-10-13 23:07:34 +0700

    Add files via upload

---
+++ matapelajaran.py
@@ -0,0 +1,61 @@
+#program matapelajaran by angga maulana
+print("   ==      ==     ===      ==    ==  ==    ")
+print("   ==  ==  ==   ==    ==   ==    ==  ==    ")
+print("   ==      ==   == -- ==   ==    ==  ==    ")
+print("   ==      ==   ==    ==   == __ ==  == = =")
+print("                                           -project.")
+y = raw_input("Masukan Nama Depan Anda      :")
+a = raw_input("Masukan Nama Belakang Anda   :")
+print "Selamat Datang..... "+y +a
+
+print('++++++++++++++++++++++++++++++++++++++++')
+print('+                                      +')
+print('+ sc by      = anggamaulana            +')
+print('+ contak     = anggasayogosm@gmail.com +')
+print('+ m.facebook = Angga Maul              +')
+print('+ version    = v 1.0		       +')
+print('++++++++++++++++++++++++++++++++++++++++')
+
+import random
+print('JADWAL PELAJARAN 12 TKJ 4 SMKN 8 KAB.TANGERANG')
+print("MASUKAN NOMOR PILIHAN ANDA     :")
+print('1.senin')
+print('2.selasa')
+print('3.rabu')
+print('4.kamis')
+print('5.jum\'at')
+print('6.info update')
+x =int(raw_input("masukan pilihan anda   :"))
+if x == 1:
+    print("=> ADM system jaringan")
+    print("=> Bahasa indonesia")
+    print("=> kewira ushaan")
+if x == 2:
+        print("=> TLJ teknologi layanan jaringan")
+        print("=> Bahasa Indonesia")
+        print("=> PJOK\(penjaskes)")
+        print("=> Pelajaran agama islam\(PAI)")
+if x == 3:
+    print("=> Bahasa Inggris")
+    print("=> Matematika")
+    print("=> ADM infrstuktur jaringan")
+if x == 4:
+    print("=> TLJ teknologi layanan jaringan")
+    print("=> PPKN")
+    print("=> ADM infrstuktur jaringan")
+    print("=> Matematika")
+if x == 5:
+    print("=> TLJ teknologi layanan jaringan")
+    print("=> ADM infrstuktur jaringan")
+    print("=> ADM system jaringan")
+if x == 6:
+    print("silahkan hubungi pembuat script..! contack di atas")
+if x > 6:
+    print("pilihan tidak tersedia coba pilih lagi..!")
+
+    
+    
+    
+    
+    
+
